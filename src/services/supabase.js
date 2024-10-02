120ed182f1aa0350943d7ef4953e83cf0570c582
Adedayoke <adedayoke2006@gmail.com>
2024-10-02 16:43:45 +0100

    cabin refactor

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://pnntvektwmvtkubgirrp.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBubnR2ZWt0d212dGt1YmdpcnJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2Mjk2MzAsImV4cCI6MjA0MzIwNTYzMH0.vcfHg67_ntmce4oAcxPrYWUQK9R3877nVIqqpt7MAJw"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
